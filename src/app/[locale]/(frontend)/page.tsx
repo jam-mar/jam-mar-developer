8d1e9fdbea441b37cd2420ce02478b380c37d46d
jamesducky <104433221+jamesducky@users.noreply.github.com>
2025-05-06 11:19:09 +0200

    refactor: remove unused imports and commented code in PayloadAdmin and Home components

--- src/app/[locale]/(frontend)/page.tsx
+++ src/app/[locale]/(frontend)/page.tsx
@@ -1,12 +1,7 @@
-import { headers as getHeaders } from 'next/headers.js'
 import Hero from '@/sections/Hero'
-import Image from 'next/image'
-import PayLoadAdmin from '@/sections/PayloadAdmin'
-import { getPayload } from 'payload'
+// import PayLoadAdmin from '@/sections/PayloadAdmin'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
-import config from '@/payload.config'
 import './styles.css'
 import '@/styles/globals.css'
 
@@ -14,17 +9,10 @@ export default async function Home({ params }: { params: Promise<{ locale: strin
   // Await the params promise
   const { locale } = await params
 
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
     <>
       <Hero locale={locale} />
-      <PayLoadAdmin locale={locale} />
+      {/* <PayLoadAdmin locale={locale} /> */}
     </>
   )
 }
