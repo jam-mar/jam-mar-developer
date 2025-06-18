d80ebfd18f44b6ee88a8d4d376947b3acaa9f581
jamesducky <104433221+jamesducky@users.noreply.github.com>
2025-06-18 11:02:48 +0200

    chore(PayLoadAdmin): remove commented-out code and clean up imports

--- src/sections/PayloadAdmin/index.tsx
+++ src/sections/PayloadAdmin/index.tsx
@@ -2,23 +2,13 @@ import Image from 'next/image'
 import React from 'react'
 import { getTranslations } from 'next-intl/server'
 import payloadConfig from '@/payload.config'
-// import { getPayload } from 'payload'
-// import { headers as getHeaders } from 'next/headers.js'
-// import config from '@/payload.config'
 interface PayLoadAdminProps {
   locale: string
 }
 
 export default async function PayLoadAdmin({ locale }: PayLoadAdminProps) {
-  // Get translations on the server
-  // TODO: Create translations
   const t = await getTranslations({ locale, namespace: 'PayLoadAdmin' })
 
-  // const headers = await getHeaders()
-  // const payloadConfig = await config
-  // const payload = await getPayload({ config: payloadConfig })
-  // const { user } = await payload.auth({ headers })
-  // const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
   return (
     <section className="home">
       <div className="content">
